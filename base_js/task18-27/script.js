d3c7e6541d7ba5a43da8fe05cd0eab37055b126a
EdemBatalow <batalow.e.i23@gmail.com>
2024-10-19 16:06:24 +0300

    task18

---
+++ base_js/task18-27/script.js
@@ -0,0 +1,31 @@
+//Задание 18_1
+let a1 = 10;
+let b1 = 2;
+
+alert('task18 ' + a1+b1)
+alert('task18 ' + a1-b1)
+alert('task18 ' + a1*b1)
+alert('task18 ' + a1/b1)
+
+//Задание 18_2
+let c = 10;
+let d = 5;
+let result= c + d;
+
+alert('task18 ' + result)
+
+//Задание 18_3
+let a3 = 1;
+let b3 = 2;
+let c3 = 3;
+
+alert('task18 ' + a3+b3+c3)
+
+//Задание 18_4
+let a4 = 10;
+let b4 = 5;
+let c4 = a4 - b4;
+let d4 = 7;
+let result4 = c4 + d4;
+
+alert('task18 ' + result4);
